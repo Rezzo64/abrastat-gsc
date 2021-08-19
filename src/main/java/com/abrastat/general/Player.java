866d6b51881e5b642e9a4820562ff9091a3da788
Rezzo64 <rezzo64@gmail.com>
2021-08-19 23:33:18 +0100

    small refactor

--- src/main/java/com/abrastat/general/Player.java
+++ src/main/java/com/abrastat/general/Player.java
@@ -2,6 +2,11 @@ package com.abrastat.general;
 
 public abstract class Player {
 
-    public static Pokemon Pokemon1, Pokemon2, Pokemon3, Pokemon4, Pokemon5, Pokemon6;
+    public static Pokemon Pokemon1,
+            Pokemon2,
+            Pokemon3,
+            Pokemon4,
+            Pokemon5,
+            Pokemon6;
 
 }
