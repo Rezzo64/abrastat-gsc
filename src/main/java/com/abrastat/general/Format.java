26019059e7edc1e4bba22d6473de0778939c27ae
Rezzo64 <rezzo64@gmail.com>
2021-08-20 01:25:37 +0100

    adding TODO

--- src/main/java/com/abrastat/general/Format.java
+++ src/main/java/com/abrastat/general/Format.java
@@ -3,6 +3,7 @@ package com.abrastat.general;
 public final class Format {
 
     //Just the first 3 for now.
+    // TODO: 20/08/2021 implement this the same way that Type.java has been. 
     public static final Format RBY = new Format();
     public static final Format GSC = new Format();
     public static final Format ADV = new Format();
