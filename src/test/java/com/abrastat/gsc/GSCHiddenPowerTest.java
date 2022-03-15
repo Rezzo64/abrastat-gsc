fc73b72dd12ed35af5d4f53c629c74905f48e8da
Rezzo64 <rezzo64@gmail.com>
2022-03-15 15:37:44 +0000

    unit tests for Hidden Power DVs

--- src/test/java/com/abrastat/gsc/GSCHiddenPowerTest.java
+++ src/test/java/com/abrastat/gsc/GSCHiddenPowerTest.java
@@ -9,6 +9,7 @@ import org.junit.jupiter.api.Test;
 import static com.abrastat.general.Type.*;
 import static com.abrastat.gsc.GSCMove.HIDDEN_POWER;
 import static org.junit.jupiter.api.Assertions.assertAll;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class GSCHiddenPowerTest {
 
@@ -91,13 +92,164 @@ class GSCHiddenPowerTest {
             .moves(HIDDEN_POWER)
             .hiddenPowerType(DARK)
             .build();
-//
-//    @Test
-//    @DisplayName("Hidden Power DVs test")
-//    void hiddenPowerDVsTest()   {
-//        assertAll("checking all individual types against the resulting DVs",
-//                () -> assertEquals());
-//    }
 
+    @Test
+    @DisplayName("Hidden Power Dark test")
+    void hiddenPowerDarkTest()   {
+        assertAll("checking Hidden Power Dark DVs are correct",
+                () -> assertEquals(15, abraDark.getIvHP()),
+                () -> assertEquals(15, abraDark.getIvAtk()),
+                () -> assertEquals(15, abraDark.getIvDef())
+                );
+    }
 
+    @Test
+    @DisplayName("Hidden Power Fire test")
+    void hiddenPowerFireTest()   {
+        assertAll("checking Hidden Power Fire DVs are correct",
+                () -> assertEquals(3, abraFire.getIvHP()),
+                () -> assertEquals(14, abraFire.getIvAtk()),
+                () -> assertEquals(12, abraFire.getIvDef())
+        );
+    }
+
+    @Test
+    @DisplayName("Hidden Power Water test")
+    void hiddenPowerWaterTest()   {
+        assertAll("checking Hidden Power Water DVs are correct",
+                () -> assertEquals(7, abraWater.getIvHP()),
+                () -> assertEquals(14, abraWater.getIvAtk()),
+                () -> assertEquals(13, abraWater.getIvDef())
+        );
+    }
+
+    @Test
+    @DisplayName("Hidden Power Electric test")
+    void hiddenPowerElectricTest()   {
+        assertAll("checking Hidden Power Electric DVs are correct",
+                () -> assertEquals(7, abraElectric.getIvHP()),
+                () -> assertEquals(14, abraElectric.getIvAtk()),
+                () -> assertEquals(15, abraElectric.getIvDef())
+        );
+    }
+
+    @Test
+    @DisplayName("Hidden Power Grass test")
+    void hiddenPowerGrassTest()   {
+        assertAll("checking Hidden Power Grass DVs are correct",
+                () -> assertEquals(3, abraGrass.getIvHP()),
+                () -> assertEquals(14, abraGrass.getIvAtk()),
+                () -> assertEquals(14, abraGrass.getIvDef())
+        );
+    }
+
+    @Test
+    @DisplayName("Hidden Power Ice test")
+    void hiddenPowerIceTest()   {
+        assertAll("checking Hidden Power Ice DVs are correct",
+                () -> assertEquals(15, abraIce.getIvHP()),
+                () -> assertEquals(15, abraIce.getIvAtk()),
+                () -> assertEquals(13, abraIce.getIvDef())
+        );
+    }
+
+    @Test
+    @DisplayName("Hidden Power Fighting test")
+    void hiddenPowerFightingTest()   {
+        assertAll("checking Hidden Power Fighting DVs are correct",
+                () -> assertEquals(3, abraFighting.getIvHP()),
+                () -> assertEquals(12, abraFighting.getIvAtk()),
+                () -> assertEquals(12, abraFighting.getIvDef())
+        );
+    }
+
+    @Test
+    @DisplayName("Hidden Power Poison test")
+    void hiddenPowerPoisonTest()   {
+        assertAll("checking Hidden Power Poison DVs are correct",
+                () -> assertEquals(3, abraPoison.getIvHP()),
+                () -> assertEquals(12, abraPoison.getIvAtk()),
+                () -> assertEquals(14, abraPoison.getIvDef())
+        );
+    }
+
+    @Test
+    @DisplayName("Hidden Power Ground test")
+    void hiddenPowerGroundTest()   {
+        assertAll("checking Hidden Power Ground DVs are correct",
+                () -> assertEquals(7, abraGround.getIvHP()),
+                () -> assertEquals(12, abraGround.getIvAtk()),
+                () -> assertEquals(15, abraGround.getIvDef())
+        );
+    }
+
+    @Test
+    @DisplayName("Hidden Power Flying test")
+    void hiddenPowerFlyingTest()   {
+        assertAll("checking Hidden Power Flying DVs are correct",
+                () -> assertEquals(7, abraFlying.getIvHP()),
+                () -> assertEquals(12, abraFlying.getIvAtk()),
+                () -> assertEquals(13, abraFlying.getIvDef())
+        );
+    }
+
+    @Test
+    @DisplayName("Hidden Power Psychic test")
+    void hiddenPowerPsychicTest()   {
+        assertAll("checking Hidden Power Psychic DVs are correct",
+                () -> assertEquals(11, abraPsychic.getIvHP()),
+                () -> assertEquals(15, abraPsychic.getIvAtk()),
+                () -> assertEquals(12, abraPsychic.getIvDef())
+        );
+    }
+
+    @Test
+    @DisplayName("Hidden Power Bug test")
+    void hiddenPowerBugTest()   {
+        assertAll("checking Hidden Power Bug DVs are correct",
+                () -> assertEquals(15, abraBug.getIvHP()),
+                () -> assertEquals(13, abraBug.getIvAtk()),
+                () -> assertEquals(13, abraBug.getIvDef())
+        );
+    }
+
+    @Test
+    @DisplayName("Hidden Power Rock test")
+    void hiddenPowerRockTest()   {
+        assertAll("checking Hidden Power Rock DVs are correct",
+                () -> assertEquals(11, abraRock.getIvHP()),
+                () -> assertEquals(13, abraRock.getIvAtk()),
+                () -> assertEquals(12, abraRock.getIvDef())
+        );
+    }
+
+    @Test
+    @DisplayName("Hidden Power Ghost test")
+    void hiddenPowerGhostTest()   {
+        assertAll("checking Hidden Power Ghost DVs are correct",
+                () -> assertEquals(11, abraGhost.getIvHP()),
+                () -> assertEquals(13, abraGhost.getIvAtk()),
+                () -> assertEquals(14, abraGhost.getIvDef())
+        );
+    }
+
+    @Test
+    @DisplayName("Hidden Power Dragon test")
+    void hiddenPowerDragonTest()   {
+        assertAll("checking Hidden Power Dragon DVs are correct",
+                () -> assertEquals(11, abraDragon.getIvHP()),
+                () -> assertEquals(15, abraDragon.getIvAtk()),
+                () -> assertEquals(14, abraDragon.getIvDef())
+        );
+    }
+
+    @Test
+    @DisplayName("Hidden Power Steel test")
+    void hiddenPowerSteelTest()   {
+        assertAll("checking Hidden Power Steel DVs are correct",
+                () -> assertEquals(15, abraSteel.getIvHP()),
+                () -> assertEquals(13, abraSteel.getIvAtk()),
+                () -> assertEquals(15, abraSteel.getIvDef())
+        );
+    }
 }
